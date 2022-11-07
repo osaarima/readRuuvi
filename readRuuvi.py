2b40d1eac56161b6dd40528041501c86d1a48df1
osaarima <oamsaarimaki@gmail.com>
2022-11-08 00:23:06 +0200

    Implemented a parser for the bluetooth output

--- readRuuvi.py
+++ readRuuvi.py
@@ -1,25 +1,54 @@
 
+#Assume format: 35.89mC where unit can be any
+#length. No space between unit and number Works 
+#´for int as well
+def findUnit(strNum):
+    pos = -1
+    posNow = 0
+    sUnit = ''
+    for iLetter in strNum:
+        if not iLetter.isnumeric():
+            if iLetter!='.':
+                sUnit += iLetter
+                if pos==-1:
+                    pos=posNow
+        posNow+=1
+
+    return (pos, sUnit)
+
 if __name__ == "__main__":
     #import sys
     import argparse
     #import os
     import subprocess
-    import json
+    from statistics import median
 
     parser = argparse.ArgumentParser(description="Reads RuuviTag information once every five minutes and saves it in a file.",
                                      epilog="Created by OS")
-
     parser.add_argument("--duration",
                         "-d",
                         help="How many seconds RuuviTags are being listened",
                         type=int,
                         nargs='?',
                         default=5)
+    parser.add_argument("--init",
+                        "-i",
+                        help="Initialize a new file",
+                        action="store_true",
+                        default=False)
+    parser.add_argument("--file",
+                        "-f",
+                        help="Filename for output",
+                        type=str,
+                        nargs='?',
+                        default="out.csv")
 
     args = parser.parse_args()
 
     print("Reading RuuviTags for {} seconds".format(args.duration))
 
+    #TODO: Check if stderr gives "Command LE Set Scan Enable execution timed out" and if so, abort.
+    #if it gives "Can't down device hci0: Device or resource busy (16" restart device?
     #Requires the installation of bluewalker
     command = 'sudo hciconfig hci0 down && sudo /home/pi/go/bin/bluewalker -device hci0 -ruuvi -duration {}'.format(args.duration)
     #os.system(command)
@@ -33,12 +62,56 @@ if __name__ == "__main__":
     lines = shOutput.split("\n")
         
     bDevice=False
+    devices    = []
+    humidities = []
+    temps      = []
+    pressures  = []
+    voltages   = []
+    units      = []
     for line in lines:
-        if not bDevice:
-            print(line[line.find("device")+7:line.find(",")])
-            bDevice=True
+        print(line)
+        splitted = line.split(' ')
+        if splitted[0].__contains__("Ruuvi"):
+            dev = line[line.find("device")+7:line.find(",")]
+            if len(devices)==0:
+                devices.append(dev)
+                humidities.append([])
+                temps.append([])
+                pressures.append([])
+                voltages.append([])
+                print(dev)
+            else:
+                if devices.count(dev)==0:
+                    devices.append(dev)
+                    humidities.append([])
+                    temps.append([])
+                    pressures.append([])
+                    voltages.append([])
+                    print(dev)
+        elif splitted[0].__contains__("Humidity"):
+            (pos, unit) = findUnit(splitted[1])
+            units.append(unit)
+            humidities[devices.index(dev)].append(float(splitted[1][:pos]))
+            (pos, unit) = findUnit(splitted[3])
+            units.append(unit)
+            temps[devices.index(dev)].append(float(splitted[3][:pos]))
+            (pos, unit) = findUnit(splitted[5])
+            units.append(unit)
+            pressures[devices.index(dev)].append(int(splitted[5][:pos]))
+            (pos, unit) = findUnit(splitted[8])
+            units.append(unit)
+            voltages[devices.index(dev)].append(int(splitted[8][:pos]))
+            
+    print(median(humidities[0]),median(humidities[1]),humidities)
+    print(temps)
+    print(pressures)
+    print(voltages)
+
+    if args.init:
+        print("Initializing a new file {}...".format(args.file))
+
+
 
     #print(shOutput)
 
-    #jsonOut = json.loads(shOutput)
 
